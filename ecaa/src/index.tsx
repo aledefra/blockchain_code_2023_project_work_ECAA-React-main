62f7decfcb86238795d11a8a307af7fdb9a2ffa7
Alessandro <defranceschi_a@yahoo.com>
2023-05-12 16:46:37 +0200

    commit cosimo

--- ecaa/src/index.tsx
+++ ecaa/src/index.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { polygonMumbai } from "wagmi/chains";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import dotenv from "dotenv";
+
+const alchemyKey = "m1UoxQUZeTSNFmdyyr_GnnVl8HBSNAcJ";
+// const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY ?? "";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+if (!alchemyKey) {
+  throw new Error("Alchemy key not found");
+}
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [polygonMumbai],
+  [alchemyProvider({ apiKey: alchemyKey })]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={client}>
+      <App />
+    </WagmiConfig>
   </React.StrictMode>
 );
 
