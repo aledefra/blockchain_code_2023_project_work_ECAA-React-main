5ad5c1fee8fcac2534caf947ed7b1517903fb1f9
CosimoRicciardi <120040607+CosimoRicciardi@users.noreply.github.com>
2023-05-22 19:44:06 +0200

    fixed connection button

--- ecaa/src/pages/headers/headers.tsx
+++ ecaa/src/pages/headers/headers.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { useAccount, useConnect } from "wagmi";
 
 export const Header = () => {
+  const { address, isConnected } = useAccount();
+  const { connect, connectors } = useConnect();
+
+
   return (
     <nav className="navbar bg-body-secondary">
       <div className="container-fluid">
@@ -13,6 +18,19 @@ export const Header = () => {
         <Link className="navbar-brand" to="/wallets/new">
           create Wallet
         </Link>
+
+        {connectors.map((connector) => (
+          <button
+          disabled={isConnected}
+            className="btn btn-lg btn-primary"
+            key={connector.id}
+            onClick={() => connect({ connector })}
+          >
+            {isConnected
+              ? "Already connected"
+              : `Connect to ${connector.name}`}
+          </button>
+        ))}
       </div>
     </nav>
   );
