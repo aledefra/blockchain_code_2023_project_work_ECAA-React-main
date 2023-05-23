e5c5520137acaf5b9b2ad6d53d2e3328b3e7ad5d
Alessandro <defranceschi_a@yahoo.com>
2023-05-23 10:46:50 +0200

    header modify

--- ecaa/src/pages/headers/headers.tsx
+++ ecaa/src/pages/headers/headers.tsx
@@ -2,36 +2,33 @@ import { Link } from "react-router-dom";
 import { useAccount, useConnect } from "wagmi";
 
 export const Header = () => {
-  const { address, isConnected } = useAccount();
-  const { connect, connectors } = useConnect();
+	const { address, isConnected } = useAccount();
+	const { connect, connectors } = useConnect();
 
+	return (
+		<nav className="navbar bg-body-secondary">
+			<div className="container-fluid">
+				<Link className="navbar-brand" to="/">
+					Homepage
+				</Link>
+				<Link className="navbar-brand" to="/wallets">
+					Your Multisigs
+				</Link>
+				<Link className="navbar-brand" to="/wallets/new">
+					Create New Wallet
+				</Link>
 
-  return (
-    <nav className="navbar bg-body-secondary">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          Homepage
-        </Link>
-        <Link className="navbar-brand" to="/wallets">
-          Multisig Wallet
-        </Link>
-        <Link className="navbar-brand" to="/wallets/new">
-          create Wallet
-        </Link>
-
-        {connectors.map((connector) => (
-          <button
-          disabled={isConnected}
-            className="btn btn-lg btn-primary"
-            key={connector.id}
-            onClick={() => connect({ connector })}
-          >
-            {isConnected
-              ? "Already connected"
-              : `Connect to ${connector.name}`}
-          </button>
-        ))}
-      </div>
-    </nav>
-  );
+				{connectors.map((connector) => (
+					<button
+						disabled={isConnected}
+						className="btn btn-lg btn-primary"
+						key={connector.id}
+						onClick={() => connect({ connector })}
+					>
+						{isConnected ? "Already connected" : `Connect to ${connector.name}`}
+					</button>
+				))}
+			</div>
+		</nav>
+	);
 };
